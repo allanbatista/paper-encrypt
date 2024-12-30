d6af3e3fc6b4fad77f1c32af50db297665570086
Allan Batista <allan@allanbatista.com.br>
2024-12-30 09:04:26 -0300

    generate multiple formats

--- paper_encrypt/main.py
+++ paper_encrypt/main.py
@@ -10,7 +10,7 @@ import cv2
 import os
 
 
-def encrypt_file(file_path, password, title=None):
+def encrypt_file(file_path, password, title=None, export_formats=['pdf']):
     if not os.path.exists(file_path):
         print(f"Error: File '{file_path}' does not exist.")
         return
@@ -26,11 +26,25 @@ def encrypt_file(file_path, password, title=None):
     encrypted_data = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
     encrypted_content = b64encode(iv + encrypted_data).decode('utf-8')
 
+    if 'txt' in export_formats:
+        output_txt = os.path.splitext(file_path)[0] + "encrypted.txt"
+        with open(output_txt, 'w+') as f:
+            f.write(encrypted_content)
+        print(f"Encrypted content saved as QR code in: {output_txt}")
+
     # Generate QR code
     qr = qrcode.QRCode()
     qr.add_data(encrypted_content)
     qr.make(fit=True)
     qr_image = qr.make_image(fill_color="black", back_color="white")
+
+    if 'img' in export_formats:
+        output_img = os.path.splitext(file_path)[0] + "encrypted.png"
+        qr_image.save(output_img)
+        print(f"Encrypted content saved as QR code in: {output_img}")
+
+    if 'pdf' not in export_formats:
+        return
 
     pdf = FPDF()
     pdf.add_page()
@@ -93,6 +107,7 @@ def main():
     parser = argparse.ArgumentParser(description="Encrypt or decrypt files using AES and QR codes.")
     parser.add_argument("--encrypt", action="store_true", help="Encrypt a file and generate a QR code in a PDF.")
     parser.add_argument("--decrypt", action="store_true", help="Decrypt a QR code from an image.")
+    parser.add_argument("--export", type=str, help="Export format. pdf|img|txt. default=pdf. multiple formats is separed by comman. --export=pdf,img,txt", default='pdf')
     parser.add_argument("--title", type=str, help="Title to add to the generated PDF.", default=None)
     parser.add_argument("file_path", type=str, help="Path to the file to encrypt or decrypt.")
 
@@ -106,10 +121,17 @@ def main():
         print("Error: You must specify either --encrypt or --decrypt.")
         return
 
+    export_formats = [x.strip() for x in args.export.lower().split(',')]
+
+    for ef in export_formats:
+        if ef not in ['pdf', 'img', 'txt']:
+            print("Error: Export is only allowed pdf, img or txt")
+            return
+
     password = getpass.getpass("Enter password: ")
 
     if args.encrypt:
-        encrypt_file(args.file_path, password, args.title)
+        encrypt_file(args.file_path, password, title=args.title, export_formats=export_formats)
     elif args.decrypt:
         decrypt_qr(args.file_path, password)
 
